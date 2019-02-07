c9e59c8c5b4f05a644852d5d057edb0ce9e610e5
Desarrollo008 <privado99999@hotmail.com>
2019-02-07 12:56:21 +0100

    MDC 003 completo

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridItemDecoration.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridItemDecoration.java
@@ -17,12 +17,19 @@ public class ProductGridItemDecoration extends RecyclerView.ItemDecoration {
         this.smallPadding = smallPadding;
     }
 
-    @Override
+    /*@Override
     public void getItemOffsets(Rect outRect, View view,
                                RecyclerView parent, RecyclerView.State state) {
         outRect.left = smallPadding;
         outRect.right = smallPadding;
         outRect.top = largePadding;
         outRect.bottom = largePadding;
+    }*/
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view,
+                               RecyclerView parent, RecyclerView.State state) {
+        outRect.left = smallPadding;
+        outRect.right = largePadding;
     }
 }
